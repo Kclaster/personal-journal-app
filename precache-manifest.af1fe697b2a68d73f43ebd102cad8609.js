13ba1749cf38b98107670d75f00a4674d9a3c875
Kyle Laster <kyle.laster@gmail.com>
2019-02-21 15:00:30 -0600

    Updates

---
+++ precache-manifest.af1fe697b2a68d73f43ebd102cad8609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b03cc100ff1cd34a8eb10f16e4a3557a",
+    "url": "/personal-journal-app/static/media/background.b03cc100.jpg"
+  },
+  {
+    "revision": "ebe8c3cb35f5e3d3d601",
+    "url": "/personal-journal-app/static/js/runtime~main.ebe8c3cb.js"
+  },
+  {
+    "revision": "da4e855401155bba1ab5",
+    "url": "/personal-journal-app/static/js/main.da4e8554.chunk.js"
+  },
+  {
+    "revision": "eb8b4f0924aac74d8604",
+    "url": "/personal-journal-app/static/js/2.eb8b4f09.chunk.js"
+  },
+  {
+    "revision": "da4e855401155bba1ab5",
+    "url": "/personal-journal-app/static/css/main.a9330045.chunk.css"
+  },
+  {
+    "revision": "5bea57b5cfbe9d14f15225fe64025b51",
+    "url": "/personal-journal-app/index.html"
+  }
+];
